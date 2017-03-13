cd5cb08dbfec66366fabd0beabc97c0194db2f18
Sarah Leahy and brendan grubb <sarah+brendan@>
2017-03-13 10:25:15 -0700

    add email form and output

---
+++ js/email-interface.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+  $('.email').submit(function(event) {
+    event.preventDefault();
+    var email = $('#email-address').val();
+    console.log(email);
+    $('#output').append("<li>" + "thanks for registering your email, " + email + "!" + "</li>");
+  })
+});
